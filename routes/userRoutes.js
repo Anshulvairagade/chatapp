764e28a84157e7ccd6f7a075f25f54e368ddb2f5
anshul <anshul_vairagade@persistent.com>
2022-02-16 21:09:56 +0530

    all commit

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  registerUser,
+  authUser,
+  allUsers,
+} = require("../controllers/userControllers");
+const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
+
+
+router.post("/login", authUser);
+
+router.route("/").post(registerUser).get(protect, allUsers);
+
+module.exports = router;
